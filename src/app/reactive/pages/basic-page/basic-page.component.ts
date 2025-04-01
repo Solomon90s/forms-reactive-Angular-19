7515197f0a1e815abeb8cb3853a556b69a0acfbd
JMHadga <jose.galdon12@gmail.com>
2025-04-01 17:57:01 +0200

    Validaciones de campos incluidos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,24 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  #formBuilder = inject(FormBuilder);
+  myForm = this.#formBuilder.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+      ] /** Validadores síncronos */ /** Validadores asíncronos */,
+    ],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+}
